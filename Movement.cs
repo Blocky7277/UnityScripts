ee444899ec5128648eb2abdd645ec4a99e966576
Blocky7277 <jacob612r@gmail.com>
2024-03-22 08:06:54 -0500

    Initial Commit

---
+++ Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float jumpForce = 1.7f;
+
+    public bool isGrounded = false;
+    public Rigidbody2D rb;
+
+    void InputHandler() {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(rb == null) rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        
+        if(Input.GetKey("space") && isGrounded) {
+            rb.velocity += new Vector2(0, jumpForce) * Time.deltaTime * 100;
+        }
+        if(Input.GetKey("a") && rb.velocity.x > -5) {
+            // if(!isGrounded && rb.velocity.x > 0) {
+            //     rb.velocity = new(0, rb.velocity.y);
+            // }
+            rb.velocity += new Vector2(-.5f, 0) * Time.deltaTime * 100;
+        }
+        
+        if(Input.GetKey("d") && rb.velocity.x < 5) {
+            // if(!isGrounded && rb.velocity.x <  0) {
+            //     rb.velocity = new(0, rb.velocity.y);
+            // }
+            rb.velocity += new Vector2(.5f, 0) * Time.deltaTime * 100;;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D obj) {
+            if (obj.gameObject.layer == 3) {
+                isGrounded = true;
+            }
+    }
+
+    void OnCollisionExit2D(Collision2D obj) {
+            if (obj.gameObject.layer == 3) {
+                isGrounded = false;
+            }
+    }
+}
